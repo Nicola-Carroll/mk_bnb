5e9cb5d7509b695a9a6c6c83a063642e7d0ab340
William Day <williamjday11@gmail.com>
2021-08-24 18:39:26 +0100

    Refined sign in method and tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -15,9 +15,18 @@ feature 'log in page' do
       password: 'griltheAnim4lz'
     )
     visit '/login'
-    fill_in('user_name', with: "Hillibilly")
+    fill_in('user_name', with: "Hilliblilly")
     fill_in('password', with: "griltheAnim4lz")
     click_button("Log In")
-    expect(page).to have_content("Welcome back to MakersBnB!")
+    expect(page).to have_content("Welcome back Willy!")
+    expect(page).to have_content("Logged in as Hilliblilly")
+  end
+
+  scenario 'checks that an non existing user is redirected to the invalid login page' do
+    visit '/login'
+    fill_in('user_name', with: "Willy")
+    fill_in('password', with: "Wally")
+    click_button("Log In")
+    expect(page).to have_content("Invalid username or password, please try again")
   end
 end
