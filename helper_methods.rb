a2f3c9c73098b1752902d60d00c79e9240addd7c
Nicola <nicolaccarroll@gmail.com>
2021-08-25 20:08:05 +0100

    user can edit availability of new listing

--- helper_methods.rb
+++ helper_methods.rb
@@ -4,3 +4,8 @@ def string_to_array(str)
   str.gsub!(/(\,)(\S)/, "\\1 \\2")
   YAML::load(str)
 end
+
+
+def unavailable_dates(params)
+  params.map { |date , status | date if status=="unavailable" }
+end
