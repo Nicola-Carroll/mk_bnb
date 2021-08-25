bf27b96bb365bf6493db022b36486077347b27f9
William Day <86121441+Willinlondon@users.noreply.github.com>
2021-08-25 13:47:04 +0100

    Will feed/register (#3)
    * Registration is set up and complete
    
    * Added the ability to view all the listed properties on a feed

--- app.rb
+++ app.rb
@@ -21,6 +21,22 @@ class Mkbnb < Sinatra::Base
     erb :index
   end
 
+  post '/register' do
+    session[:new_user] = User.create!(
+      first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      email: params[:email], 
+      user_name: params[:user_name], 
+      password: params[:password]
+    )
+    redirect '/register'
+  end
+
+  get '/register' do
+    @new_user = session[:new_user]
+    erb :register
+  end
+
   get '/login' do
     erb :login
   end
@@ -50,7 +66,7 @@ class Mkbnb < Sinatra::Base
     Room.create!(
       title: params[:title],
       description: params[:description],
-      price_per_night:params[:price_per_night].to_f,
+      price_per_night: params[:price_per_night].to_f,
       user_id: session[:current_user].id
     )
     @rooms = Room.all
