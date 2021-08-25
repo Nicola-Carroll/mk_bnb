9e4e123d73007c4e20b5be61574079007a650c96
Nicola <nicolaccarroll@gmail.com>
2021-08-25 16:02:39 +0100

    adding availability to database

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_08_25_132549) do
     t.string "booking_status", limit: 20, null: false
     t.index ["room_id"], name: "index_requests_on_room_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
+
+  create_table "available_dates", force: :cascade do |t|
+    t.date "start_date", null: false
+    t.date "end_date"
+    t.integer "rooms_id", null: false
+    t.index ["rooms_id"], name: "index_available_dates_on_rooms_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -25,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_132549) do
     t.string "description", null: false
     t.float "price_per_night", null: false
     t.integer "user_id", null: false
+    t.string "availability"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
