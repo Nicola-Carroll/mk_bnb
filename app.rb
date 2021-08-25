dd44ce1cb6073f5c9a87f2192a15379c57889346
Nicola <nicolaccarroll@gmail.com>
2021-08-25 20:37:38 +0100

    not sure what i changed lol

--- app.rb
+++ app.rb
@@ -110,7 +110,7 @@ class Mkbnb < Sinatra::Base
   get '/requests' do
     erb :requests
   end
-  
+
   # get '/edit_listing' do
   #   @availability = string_to_array(Room.last.availability)
   #   erb :edit_listing
