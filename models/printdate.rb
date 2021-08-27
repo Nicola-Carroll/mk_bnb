b7ccf7b5fb1f3b5718db00fc5d3200dbcc1a8e4f
Nicola <nicolaccarroll@gmail.com>
2021-08-27 12:56:04 +0100

    method in print date doesn't work

--- models/printdate.rb
+++ models/printdate.rb
@@ -1,9 +1,8 @@
 require 'date'
 
 class PrintDate
-  def self.selected_date(date)
-    string = date.split('-').map(&:to_i)
-    new_date = Date.new(string[0], string[1], string[2])
-    new_date.strftime("%d %B %Y")
+  def self.selected_date(date_string)
+    date = Date.strptime(date_string,"%Y-%m-%d")
+    date.strftime("%d %B %Y")
   end
 end
